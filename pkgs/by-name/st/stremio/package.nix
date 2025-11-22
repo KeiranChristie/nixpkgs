d7c595ade898fc5a17342e53ec7884e963a910b3
KeiranChristie <KeiranJChristie@outlook.com>
2025-11-22 01:24:26 +0000

    stremio: fix sed insertion for WebEngine find_package calls
    Use separate sed -i commands to insert each line individually,
    inserting in reverse order to maintain correct line numbers.

--- pkgs/by-name/st/stremio/package.nix
+++ pkgs/by-name/st/stremio/package.nix
@@ -97,11 +97,18 @@ stdenv.mkDerivation (finalAttrs: {
           echo "Found WebEngine near find_package, inserting find_package for WebEngine components"
           # Insert find_package calls for WebEngine components before the Qt6 find_package line
           # Qt6 WebEngine is split into components that need to be found separately
+          # Insert lines in reverse order to maintain correct line numbers
+          sed -i "''${QT6_LINE}i\\
+find_package(Qt6WebEngineQuick REQUIRED)\\
+" CMakeLists.txt
+          sed -i "''${QT6_LINE}i\\
+find_package(Qt6WebEngineWidgets REQUIRED)\\
+" CMakeLists.txt
+          sed -i "''${QT6_LINE}i\\
+find_package(Qt6WebEngineCore REQUIRED)\\
+" CMakeLists.txt
           sed -i "''${QT6_LINE}i\\
 # Find Qt6WebEngine components separately (Nixpkgs has them as separate packages)\\
-find_package(Qt6WebEngineCore REQUIRED)\\
-find_package(Qt6WebEngineWidgets REQUIRED)\\
-find_package(Qt6WebEngineQuick REQUIRED)\\
 " CMakeLists.txt
           
           # Now remove WebEngine from the Qt6 find_package call (check next 10 lines)
